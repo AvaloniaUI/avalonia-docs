eaef2f088a6b880981dc5ceb991973484791c83b
Mike James <michael.james6@gmail.com>
2023-04-24 13:29:44 +0200

    Updated header title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentation',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
