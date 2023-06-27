7287be7eb8c5d62b46c9fb44ab4b220b47571888
Mike James <michael.james6@gmail.com>
2023-06-27 16:19:20 +0200

    More social card tweaking.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/avaloniaui-social-card.jpg',
+      image: 'img/social-card.png',
       announcementBar: {
         id: 'support_us',
         content:
