6e0d1eb6b47c53c789c3e0597f34b14f6c7951a3
Mike James <michael.james6@gmail.com>
2023-06-28 16:41:25 +0200

    Fixed broken 'edit this page' links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/AvaloniaUI/Docs',
+          editUrl: 'https://github.com/AvaloniaUI/avalonia-docs/tree/main',
           //includeCurrentVersion: false,
           //lastVersion: '0.10.x',
           versions: {
