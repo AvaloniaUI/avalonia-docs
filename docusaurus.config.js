cbec82ab249e05ba35196cea38720b9fded88db8
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-11-16 18:56:42 +0530

    Update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -239,8 +238,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} AvaloniaUI OÜ (14839404)`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
         additionalLanguages: ['csharp'],
       },
     }),
