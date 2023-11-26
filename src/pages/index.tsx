60207dd82237e54c6dc5bbc2d7c1861fe64e75db
Max Katz <maxkatz6@outlook.com>
2023-11-25 16:30:20 -0800

    Remove "/next/" from the links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/next/welcome">
+            to="/docs/welcome">
             Get Started! 
           </Link>
        
