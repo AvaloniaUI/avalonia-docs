230338a740c54968f72cd3c4c37ccf52b1b1b704
Max Katz <maxkatz6@outlook.com>
2023-11-25 20:52:06 -0800

    Add missing dev tools page to 11.0

--- sidebars.js
+++ sidebars.js
@@ -266,6 +266,7 @@ const sidebars = {
           'items': [
             'guides/implementation-guides/how-to-use-the-mvvm-pattern',
             'guides/implementation-guides/how-to-implement-dependency-injection',
+            'guides/implementation-guides/developer-tools',
             'guides/implementation-guides/logging-errors-and-warnings',
             'guides/implementation-guides/ide-support',
             'guides/implementation-guides/how-to-use-design-time-data',
