492b48f88c656bf973c7ceac0ce92e9a632e9b67
Steve Monaco <1782158+stevemonaco@users.noreply.github.com>
2024-04-16 17:13:42 -0400

    Change dark theme for code blocks to vsDark

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer').themes.github;
-const darkCodeTheme = require('prism-react-renderer').themes.dracula;
+const darkCodeTheme = require('prism-react-renderer').themes.vsDark;
 
 const fs = require('fs');
 const resourcesHTML = fs.readFileSync('./src/snippets/resources.html', 'utf-8');
