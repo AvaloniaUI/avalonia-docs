0eeb6cdb18b3b7cd804da1775068047cc1f93677
Luke <55367595+luke-whos-here@users.noreply.github.com>
2025-11-11 17:04:21 +0000

    Try adding all URLs as list
    The last attempt to redirect didn't work, so I'm trying to add all MusicStoreApp tutorial pages as a list.

--- redirects/index.js
+++ redirects/index.js
@@ -22,7 +22,26 @@ const config = {
     { from: '/accelerate/tools/parcel/guies/apple-notary', to: '/accelerate/tools/parcel/apple/signing' },
     { from: '/accelerate/tools/parcel/guies/apple-signing', to: '/accelerate/tools/parcel/apple/notary' },
     // MusicStoreApp tutorial migrated to Avalonia.Samples
-    { from: '/docs/tutorials/music-store-app/', to: '/music-store-redirect.html'}
+    { to: '/music-store-redirect.html',
+      from: [
+            '/docs/tutorials/music-store-app/',
+            '/docs/tutorials/music-store-app/creating-the-project',
+            '/docs/tutorials/music-store-app/creating-a-modern-looking-window',
+            '/docs/tutorials/music-store-app/add-and-layout-controls',
+            '/docs/tutorials/music-store-app/button-command',
+            '/docs/tutorials/music-store-app/opening-a-dialog',
+            '/docs/tutorials/music-store-app/add-content-to-dialog',
+            '/docs/tutorials/music-store-app/mock-search',
+            '/docs/tutorials/music-store-app/album-view',
+            '/docs/tutorials/music-store-app/searching-for-albums',
+            '/docs/tutorials/music-store-app/displaying-images',
+            '/docs/tutorials/music-store-app/return-from-dialog',
+            '/docs/tutorials/music-store-app/add-items-to-users-collection',
+            '/docs/tutorials/music-store-app/add-data-persistence',
+            '/docs/tutorials/music-store-app/load-data-at-startup',
+            '/docs/tutorials/music-store-app/summary',
+          ],
+    },
   ],
   createRedirects(existingPath) {
     const redirects = from10to11.createRedirects(existingPath) || [];
@@ -33,6 +52,11 @@ const config = {
 
     console.log(existingPath, redirects);
     return redirects.length ? redirects : undefined;
+  },
+  createRedirects(existingPath) {
+    if (existingPath.includes('/docs/tutorials/music-store-app/')) {
+
+    } 
   },
 };
 
