e079bb0ed34349ef54659e7140c44abee1ae91af
Luke <55367595+luke-whos-here@users.noreply.github.com>
2025-11-11 14:08:39 +0000

    Create HTML redirect
    Looks like plugin-client-redirects doesn't allow offsite redirects. Going to try adding a static HTML page with a refresh header.

--- redirects/index.js
+++ redirects/index.js
@@ -22,7 +22,7 @@ const config = {
     { from: '/accelerate/tools/parcel/guies/apple-notary', to: '/accelerate/tools/parcel/apple/signing' },
     { from: '/accelerate/tools/parcel/guies/apple-signing', to: '/accelerate/tools/parcel/apple/notary' },
     // MusicStoreApp tutorial migrated to Avalonia.Samples
-    { from: '/docs/tutorials/music-store-app/', to: 'https://github.com/AvaloniaUI/Avalonia.Samples/tree/main/src/Avalonia.Samples/CompleteApps/Avalonia.MusicStore'}
+    { from: '/docs/tutorials/music-store-app/', to: '/music-store-redirect.html'}
   ],
   createRedirects(existingPath) {
     const redirects = from10to11.createRedirects(existingPath) || [];
