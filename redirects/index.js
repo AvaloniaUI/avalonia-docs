1a46ee5e4d050854e7ad956d257f76dd81e74d7d
Luke <55367595+luke-whos-here@users.noreply.github.com>
2025-11-12 12:24:59 +0000

    Optimizing file structure
    To try and keep the files neat, I'm moving the list of redirects to its own js file. Also moving the redirect html page into a folder, in case we need more of these in the future.

--- redirects/index.js
+++ redirects/index.js
@@ -2,11 +2,13 @@
 // can be tested with this script https://gist.github.com/maxkatz6/760bc0615b1af2ead4bf1f0582fdcd10
 
 import {from10to11} from './migrate-010-to-110.js';
+import {music_store_redirects} from './music-store-redirect.js';
 
 const config = {
   redirects: [
 
     ...from10to11.redirects,
+    ...music_store_redirects.redirects,
 
     // 'contro' typo
     { from: '/docs/next/basics/user-interface/controls/creating-controls/choosing-a-custom-contro-type', to: '/docs/basics/user-interface/controls/creating-controls/choosing-a-custom-control-type' },
@@ -21,27 +23,6 @@ const config = {
     // Parcel invalid links
     { from: '/accelerate/tools/parcel/guies/apple-notary', to: '/accelerate/tools/parcel/apple/signing' },
     { from: '/accelerate/tools/parcel/guies/apple-signing', to: '/accelerate/tools/parcel/apple/notary' },
-
-    { to: 'pathname:///music-store-redirect.html',
-      from: [
-            '/docs/tutorials/music-store-app/',
-            '/docs/tutorials/music-store-app/creating-the-project',
-            '/docs/tutorials/music-store-app/creating-a-modern-looking-window',
-            '/docs/tutorials/music-store-app/add-and-layout-controls',
-            '/docs/tutorials/music-store-app/button-command',
-            '/docs/tutorials/music-store-app/opening-a-dialog',
-            '/docs/tutorials/music-store-app/add-content-to-dialog',
-            '/docs/tutorials/music-store-app/mock-search',
-            '/docs/tutorials/music-store-app/album-view',
-            '/docs/tutorials/music-store-app/searching-for-albums',
-            '/docs/tutorials/music-store-app/displaying-images',
-            '/docs/tutorials/music-store-app/return-from-dialog',
-            '/docs/tutorials/music-store-app/add-items-to-users-collection',
-            '/docs/tutorials/music-store-app/add-data-persistence',
-            '/docs/tutorials/music-store-app/load-data-at-startup',
-            '/docs/tutorials/music-store-app/summary',
-          ],
-    },
   ],
   createRedirects(existingPath) {
     const redirects = from10to11.createRedirects(existingPath) || [];
