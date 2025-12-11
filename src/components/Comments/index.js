d6bdf4c0efc2bf7c09d920dba47f15910d6a674f
Luke <55367595+luke-whos-here@users.noreply.github.com>
2025-12-11 16:46:48 +0000

    Versatile colour mode
    Make the comments box match the dark/light setting of the site.

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -18,7 +18,7 @@ export default function Comments() {
         reactionsEnabled="1"
         emitMetadata="0"
         inputPosition="top"
-        theme="dark"
+        theme={colorMode}
         lang="en"
         loading="lazy"
       />
