afcf4e0113eeafcff66861bad4d1a5de67b40a43
Luke <55367595+luke-whos-here@users.noreply.github.com>
2026-09-23 11:03:49 +0100

    Generate new API docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -312,7 +312,7 @@ gtag('consent', 'default', {
         routeBasePath: "api",
         disableVersioning: true,
         sidebarPath: require.resolve("./api-sidebars.ts"),
-        remarkPlugins: [[require("./plugins/apiref-xref"), { indexPath: "./dist/xref/12.1.2.1.xrefmap.json" }]],
+        remarkPlugins: [[require("./plugins/apiref-xref"), { indexPath: "./dist/xref/12.1.3.xrefmap.json" }]],
       },
     ]
   ],
